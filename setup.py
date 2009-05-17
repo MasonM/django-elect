1265d573ea06daa7b2dd9ba6f526e35c9e3969f3
mrman@masons-comp <mrman@masons-comp>
2009-05-17 19:45:56 -0400

    Adding ez_setup.py and changing setup.py. Added MANIFEST.in and fixing package data config.

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
+from ez_setup import use_setuptools
+use_setuptools()
 from setuptools import setup, find_packages
 
 setup(name='django-elect',
     version='0.1',
     description='A simple voting app for Django, mainly intended for '+\
                 'academic organizations.',
+    license='BSD',
     author='Mason Malone',
     author_email='mason.malone@gmail.com',
     url='http://bitbucket.org/MasonM/django-elect/',
-    packages=find_packages(exclude=['example_project']),
+    packages=find_packages(exclude=['example_project', 'example_project.*']),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
